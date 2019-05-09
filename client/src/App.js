2f670e05bb48f6f25981c1b1a2c713a087a088ac
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2019-05-09 07:45:48 +0530

    working

--- client/src/App.js
+++ client/src/App.js
@@ -5,25 +5,29 @@ import './App.css';
 
 
 
-class Word extends React.Component{
+class Mapping extends React.Component{
   constructor(props){
     super(props);
     // const data = this.props.data.word;
     this.state = {
-      word: props.data.word
+      // word: props.data.word
     }
   }
 
-  change = (evt)=>{
-    this.setState({word: this.props.data.value})
-  }
-
-  reset = (evt)=>{
-    this.setState({word: this.props.data.word})
-  }
-
   render(){
-    return (<span onMouseEnter={this.change} onMouseLeave={this.reset} className={"word "+ this.props.data.label}>{this.state.word}</span>);
+    const arr = [];
+    // console.log(this.props);
+    for(const x in this.props.data){
+      // const score = (<span class="score"> {this.props.data.score} </span>);
+      if(!x.length) continue;
+      const label = (Number(this.props.data[x]['score']) > 0)?"positive":"negative";
+      for(const y of this.props.data[x]["targets"]){
+        if(!y.length) continue;
+        arr.push((<span class={"mapping "+label}><span class="x">{x}</span> &rarr; <span class="y">{y}</span></span>));
+      }
+    }
+    return (<div>{arr}</div>);
+    // return (<span onMouseEnter={this.change} onMouseLeave={this.reset} className={"word "+ this.props.data.label}>{this.state.word}</span>);
   }
 }
 
@@ -35,31 +39,38 @@ class Row extends React.Component{
   }
 
   render(){
-    const sentence = this.props.data.map((value, idx)=>(<Word data = {value} key={idx}/>));
-    return (<span className={"row"}>{sentence}</span>);
+    const result = this.props.data.result;
+    const sentence = this.props.data.sentence;
+    return (<div><span class={"sentence"}> {sentence} </span><br/><Mapping data = {result}/></div>);
   }
 }
 
 class MyTable extends React.Component{
   constructor(){
     super();
-    this.state = {
-      data : [],
-      line: "",
-      url: ""
+    let res = window.localStorage.getItem('data');
+    if(res==null){
+      window.localStorage.setItem("data", JSON.stringify([]));
     }
+    res = JSON.parse(window.localStorage.getItem('data'));
+    this.state = {
+      data : res,
+      line: "",
+      url: "http://localhost/text/"
+    }
+    
   }
 
-  readInput = (evt)=>{
-    const file = evt.target.files[0];
-    const fileReader = new FileReader();
-    fileReader.readAsText(file);
-    fileReader.onload = (evt) => this.setState({data: JSON.parse(fileReader.result)});
+  // readInput = (evt)=>{
+  //   const file = evt.target.files[0];
+  //   const fileReader = new FileReader();
+  //   fileReader.readAsText(file);
+  //   fileReader.onload = (evt) => this.setState({data: JSON.parse(fileReader.result)});
 
-  }
-  ReadFile = ()=>{
-   return (<input type="file" onInput={this.readInput}></input>)
-  }
+  // }
+  // ReadFile = ()=>{
+  //  return (<input type="file" onInput={this.readInput}></input>)
+  // }
 
   readLine = (evt)=>{
     this.setState({line: evt.target.value.trim()});
@@ -68,46 +79,41 @@ class MyTable extends React.Component{
   getResult = (evt)=>{
     evt.preventDefault();
     if(this.state.line.trim().length == 0) return
-    const res = [
-      {
-          "word": "Hey",
-          "label": "positive",
-          "value": 0.564
-      },
-      {
-          "word": "I",
-          "label": "positive",
-          "value": 0.564
-      },
-      {
-          "word": "am",
-          "label": "positive",
-          "value": 0.564
-      },
-      {
-          "word": "good",
-          "label": "positive",
-          "value": 0.564
-      }
-  ];
-  fetch(this.state.url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, cors, *same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-        "Content-Type": "application/json",
-        // "Content-Type": "application/x-www-form-urlencoded",
-    },
-    redirect: "follow", // manual, *follow, error
-    referrer: "no-referrer", // no-referrer, *client
-    body: JSON.stringify({line: this.state.line}), // body data type must match "Content-Type" header
-  }).then((res)=>{
-    console.log(res);
+    // const res = {"sentence": "toilet are bad", "result": {"toilet": {"targets": ["bad"], "score": "-2.000000"}}};
+
+    
+
+  //   fetch(this.state.url+this.state.line, {
+  //   method: "GET", // *GET, POST, PUT, DELETE, etc.
+  //   mode: "cors", // no-cors, cors, *same-origin
+  //   // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+  //   credentials: "same-origin", // include, *same-origin, omit
+  //   // headers: {
+  //   //     "Content-Type": "application/json",
+  //   //     // "Content-Type": "application/x-www-form-urlencoded",
+  //   // },
+  //   // redirect: "follow", // manual, *follow, error
+  //   // referrer: "no-referrer", // no-referrer, *client
+  //   // body: JSON.stringify({line: this.state.line}), // body data type must match "Content-Type" header
+  // })
+  // fetch("http://localhost/text/toilets%20stink%20and%20basin%20leak%20water", 
+  fetch(this.state.url+this.state.line, {"headers":{"accept":"application/json","cache-control":"no-cache","pragma":"no-cache","upgrade-insecure-requests":"1"},"referrerPolicy":"no-referrer-when-downgrade","method":"GET","dataType": "jsonp"})
+  .then((r)=>{
+    r.json().then((res)=>{
+      const arr = this.state.data.slice();
+      arr.push(res);
+    // console.log(arr);
+    this.setState({data: arr});
+    console.log(arr);
+    window.localStorage.setItem("data", JSON.stringify(arr));
+    });
   });
-  const arr = this.state.data.slice();
-  arr.push(res);
-  this.setState({data: arr});
+    // const arr = this.state.data.slice();
+    // arr.push(res);
+    // console.log(arr);
+    // this.setState({data: arr});
+    // console.log(JSON.stringify(this.state.data))
+    // window.localStorage.setItem("data", JSON.stringify(arr));
   }
 
   TakeInput = ()=>{
@@ -119,26 +125,28 @@ class MyTable extends React.Component{
       </div>);
   }
 
-  urlChange = (evt)=>{
-    this.setState({url: evt.target.value.trim()});
-  }
+  // urlChange = (evt)=>{
+  //   this.setState({url: evt.target.value.trim()});
+  // }
 
-  saveData = (evt)=>{
+  // saveData = (evt)=>{
     
-  }
+  // }
 
   render() {
-    const rows = this.state.data.reverse().map((value, idx)=>(<div className="line" key={idx} ><Row data = {value}/></div>));
+    const rows = this.state.data.map((value, idx)=>
+      (<div className="line" key={idx} ><Row data = {value}/></div>)
+    );
     return (
       <div>
-        <input type="text" onChange={this.urlChange}/>
+        {/* <input type="text" onChange={this.urlChange}/> */}
         <this.TakeInput/>
         <br/>
-        <this.ReadFile/>
+        {/* <this.ReadFile/> */}
         <br/>
         {rows}
         <br/>
-        <input type="button" value="save" onClick={this.saveData} />
+        {/* <input type="button" value="save" onClick={this.saveData} /> */}
       </div>
     )
   }
@@ -147,6 +155,7 @@ class MyTable extends React.Component{
 function App() {
   return (
     <div className="App">
+    <h1>Sentiment Analyzer</h1>
       <MyTable />
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
--- server/server.py
+++ server/server.py
@@ -2,8 +2,11 @@ from flask import Flask, redirect
 # from analyzer import analyze
 import os, json
 from dummy import output
+from flask_cors import CORS
 
-host = '0.0.0.0'
+
+
+host = 'localhost'
 if "IP" in os.environ.keys():
 	host = os.environ["IP"]
 
@@ -12,7 +15,7 @@ if "PORT" in os.environ.keys():
 	port = os.environ["PORT"]
 
 app = Flask(__name__,static_folder="./client/",static_url_path='')
-
+CORS(app)
 # object = analyze()
 
 @app.route('/',methods=["GET"])
@@ -20,8 +23,9 @@ def main_page():
         return 	"hello"
         return redirect('index.html')
 
-@app.route('/text/<text>',methods=["GET","POST"])
+@app.route('/text/<text>',methods=["GET"])
 def sentiment_analyze(text):
+	# print("HERE")
 	#text = 'toilets are so bad that they stink like anything and basin is leaking'
 	res = output(text)
  #    res = json.loads("""{'resultid': 'c5d38c59-46b7-4600-a7ab-a4b746432e26',                                                             
@@ -44,6 +48,7 @@ def sentiment_analyze(text):
 	#    'text_norm': 'be,leak',                                                                                       
 	#    'score': '-1.000000'}]}                                                                                                                                                                                            
 	# """)
+	print(text)
 	obj = {}
 	for i in res['sentimentanalysis']:
 		topics = i['topic'].split(',')
@@ -57,6 +62,12 @@ def sentiment_analyze(text):
 	ret['sentence'] = text
 	ret['result'] = obj
 	print(ret)
-	return json.dumps(ret)
+	response = app.response_class(
+        response=json.dumps(ret),
+        mimetype='application/json',
+		status=200,
+    )
+	# response.headers['Access-Control-Allow-Origin'] = "*"
+	return response
 
 app.run(host=host,port=port,debug=True)
